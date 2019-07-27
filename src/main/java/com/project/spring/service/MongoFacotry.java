786842b795aac75c33899875957feeda5c32751d
hbnking <hbn.king@gmail.com>
2019-07-27 17:24:38 +0800

    back

---
+++ src/main/java/com/project/spring/service/MongoFacotry.java
@@ -0,0 +1,9 @@
+package com.project.spring.service;
+
+/**
+ * @author wangheng
+ * @date 2019-06-13 上午11:58
+ * @description
+ **/
+public class T {
+}
---
+++ src/main/java/com/project/spring/service/Test.java
@@ -0,0 +1,9 @@
+package com.project.spring.service;
+
+/**
+ * @author wangheng
+ * @date 2019-06-13 上午11:56
+ * @description
+ **/
+public class Test {
+}
